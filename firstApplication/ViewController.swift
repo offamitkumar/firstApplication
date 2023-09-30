0e3bf8fe879da5bcb12e0471ad48ee392b602673
Amit Kumar <amitlocham09@icloud.com>
2023-09-30 17:00:05 +0530

    Initial Commit

---
+++ firstApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstApplication
+//
+//  Created by Amit Kumar on 30/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

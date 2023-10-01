41864dac7e59f0104b377036a01b6ea0d156cd21
Amit Kumar <amitlocham09@icloud.com>
2023-10-01 07:52:21 +0530

    loading all files

--- firstApplication/ViewController.swift
+++ firstApplication/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var firstImage:Bool = true
+    @IBOutlet weak var ImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changeImage(_ sender: Any) {
+        if (firstImage) {
+            ImageView.image = UIImage(named: "image2")
+        } else {
+            ImageView.image = UIImage(named: "Image_1")
+        }
+        
+        firstImage = (!firstImage)
+        print("button pressed")
+    }
+    
 }
 
